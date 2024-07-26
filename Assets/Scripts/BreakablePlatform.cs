98a3054d3fc3de4fdc353b54b9e0b89f5d0f2611
ErenYZR <erenyazar16@gmail.com>
2024-07-26 12:35:39 +0300

    Breakable Platform

---
+++ Assets/Scripts/BreakablePlatform.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakablePlatform : MonoBehaviour
+{
+	[SerializeField] private float delay;
+	PlayerMovement playerMovement;
+	[SerializeField] GameObject player;
+
+
+	private void Awake()
+	{
+		playerMovement = player.GetComponent<PlayerMovement>();
+	}
+
+	private void Update()
+	{
+		if(playerMovement.dead == true)
+		{
+			transform.localScale = Vector3.one;
+		}
+	}
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		if(collision.transform.tag == "Player")
+		{
+			StartCoroutine(Breaking());
+		}
+	}
+
+	private IEnumerator Breaking()
+	{
+
+		yield return new WaitForSeconds(delay);
+		transform.localScale = Vector3.zero;
+	}
+}
