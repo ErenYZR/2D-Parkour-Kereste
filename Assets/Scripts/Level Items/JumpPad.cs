810f439f6042f0d6686c8d6daab67f42de90232f
ErenYZR <erenyazar16@gmail.com>
2024-12-20 17:17:53 +0300

    Jump pad dash reset

--- Assets/Scripts/Level Items/JumpPad.cs
+++ Assets/Scripts/Level Items/JumpPad.cs
@@ -49,6 +49,7 @@ public class JumpPad : MonoBehaviour
 	{
 		if (collision.gameObject.CompareTag("Player"))
 		{
+			playerMovement.canDashCondition = true;
 			playerMovement.isWallJumping = false;
 			if (bounceCoroutine != null) StopCoroutine(bounceCoroutine);
 			bounceCoroutine = Bounce();
