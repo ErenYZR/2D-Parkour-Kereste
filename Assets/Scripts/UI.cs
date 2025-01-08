8a59d4f044609ff642b8b2bd7e67d827928ca819
ErenYZR <erenyazar16@gmail.com>
2025-01-08 20:57:05 +0300

    Level 3.1

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class UI : MonoBehaviour
 {
-    public Text text;
+    [SerializeField] TextMeshProUGUI text;
     private float timer;
 
     // Start is called before the first frame update
