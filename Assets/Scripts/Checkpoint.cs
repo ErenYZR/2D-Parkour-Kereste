8536b4435107369d19f86b14be767ed8769f431f
ErenYZR <erenyazar16@gmail.com>
2025-02-04 23:49:04 +0300

    Checkpoint sign

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -5,9 +5,21 @@ using UnityEngine;
 public class Checkpoint : MonoBehaviour
 {
 	Respawn respawn;
+
+	public static Checkpoint activePoint;
+	[SerializeField] private Sprite redCheckpoint;
+	[SerializeField] private Sprite greenCheckpoint;
+	private SpriteRenderer spriteRenderer;
+
 	private void Awake()
 	{
 		respawn = GameObject.FindGameObjectWithTag("Player").GetComponent<Respawn>();
+		spriteRenderer = GetComponent<SpriteRenderer>();
+	}
+
+	private void Start()
+	{
+		
 	}
 
 	private void OnTriggerEnter2D(Collider2D collision)
@@ -15,6 +27,24 @@ public class Checkpoint : MonoBehaviour
 		if (collision.CompareTag("Player"))
 		{
 			respawn.UpdateCheckpoint(transform.position);
+
+			if(activePoint != null)
+			{
+				activePoint.SetToRed();
+			}
+
+			activePoint = this;
+			SetToGreen();
 		}
+	}
+
+	void SetToGreen()
+	{
+		spriteRenderer.sprite = greenCheckpoint;
+	}
+
+	void SetToRed()
+	{
+		spriteRenderer.sprite = redCheckpoint;
 	}
 }
